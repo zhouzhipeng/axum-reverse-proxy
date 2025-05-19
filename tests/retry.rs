009b9ab2f3ff86733095a4b9c6b4417133972ca6
Tom Lubenow <39017419+tom-lubenow@users.noreply.github.com>
2025-05-18 20:36:40 -0700

    Ensure RetryLayer uses at least one attempt

--- tests/retry.rs
+++ tests/retry.rs
@@ -73,3 +73,34 @@ async fn test_retry_layer() {
     proxy_server.abort();
     server_handle.abort();
 }
+
+#[tokio::test]
+async fn test_retry_layer_zero_attempts() {
+    let temp = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = temp.local_addr().unwrap();
+    drop(temp);
+
+    let proxy = ReverseProxy::new("/", &format!("http://{}", addr));
+    let app: Router = proxy.into();
+    let app = app.layer(ServiceBuilder::new().layer(RetryLayer::new(0)));
+
+    let proxy_listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let proxy_addr = proxy_listener.local_addr().unwrap();
+    let proxy_server = tokio::spawn(async move {
+        axum::serve(proxy_listener, app).await.unwrap();
+    });
+
+    let server_handle = tokio::spawn(delayed_server(addr));
+
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(format!("http://{}/test", proxy_addr))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(resp.status(), reqwest::StatusCode::BAD_GATEWAY);
+
+    proxy_server.abort();
+    server_handle.abort();
+}
